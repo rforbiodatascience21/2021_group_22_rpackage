844a50de6f566670d8f1c54f303d8b4c20b187b1
albanobel <albanobel@gmail.com>
2021-03-22 09:38:08 +0000

    function update

--- R/random_dna_function.R
+++ R/random_dna_function.R
@@ -1,7 +1,7 @@
 #' R/random_dna.R
 #' Sample random string of nucleotides of size l
 #' input: l, length
-#' return: nulceotides
+#' return: nucleotides
 random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
